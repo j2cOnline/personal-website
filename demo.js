11c73c4641535c22ea7114d32885da16715b53fa
j2c2003 <j2c2003@yahoo.com>
2019-04-10 22:41:20 -0700

    added web dev section to projects

--- demo.js
+++ demo.js
@@ -1,4 +1,4 @@
-const demo = `<div class="newDiv main-body body">
+var demo = `<div class="newDiv main-body body">
                 <div class="demo-div">      
                 <h1>Coming Soon...</h1>
                      <a target="_blank" href="https://youtu.be/_ZCl8_3_N6I"><img class="magic-gif"  src="img/MagicTouchClip.gif" alt="gameplay of magic touch"></a>
